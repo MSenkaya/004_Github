81ffba5b0d3495b4ff69c9a93ac08dabe0595bb3
Mustafa <senkaya@gmail.com>
2020-08-17 14:53:06 +0300

    Güncelleme

--- 004_Github/src/main/java/com/senkaya/_Github/App.java
+++ 004_Github/src/main/java/com/senkaya/_Github/App.java
@@ -3,7 +3,9 @@ package com.senkaya._Github;
 public class App 
 {
     public static void main( String[] args )
-    {
+    {	
+    	System.out.println( "-------------" );
         System.out.println( "Hello Github!" );
+        System.out.println( "-------------" );
     }
 }
